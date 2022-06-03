1d7a3d3fe56abbc41c45346debcb98f7795da6c0
Emmanuel Chassot <EChassot@iotc.local>
2022-06-03 14:23:48 +0400

    remove useless folders and changed name of data set

--- initialization/02_DATA_READING.R
+++ initialization/02_DATA_READING.R
@@ -3,6 +3,6 @@ library(iotc.base.common.plots)
 
 print("Read morphometric data...")
 
-MORPHOMETRICS_TABLE = fread("../inputs/data/ALB_FORK_LENGTH_ROUND_WEIGHT_DATASET.csv")
+ALB_FL_RD = fread("../inputs/data/ALB_FORK_LENGTH_ROUND_WEIGHT_DATASET.csv")
 
 print("Morphometric data read!")
