0274cd42793a6f7cdf3fdc7d101a19795f260e68
Emmanuel Chassot <EChassot@iotc.local>
2022-06-06 07:23:26 +0400

    change of file to read

--- initialization/02_DATA_READING.R
+++ initialization/02_DATA_READING.R
@@ -3,6 +3,6 @@ library(iotc.base.common.plots)
 
 print("Read morphometric data...")
 
-ALB_FL_RD = fread("../inputs/data/ALB_FORK_LENGTH_ROUND_WEIGHT_DATASET.csv")
+ALB_FL_RD = fread("../inputs/data/ALB_FORK_LENGTH_ROUND_WEIGHT_DATASET_CWP55.csv")
 
 print("Morphometric data read!")
